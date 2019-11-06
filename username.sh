7cc08f948cec798ae89aa40bd25bb6b25a6b351e
Luke Driscoll <ldriscoll@chapman.edu>
2019-11-06 10:45:51 -0800

    Adds username.sh

--- username.sh
+++ username.sh
@@ -3,7 +3,7 @@
 # Luke Driscoll
 echo "Enter a 3-12 character username with only lower case letters, digits and underscores"
 read USERNAME
-while echo "$USERNAME" | egrep -v "([a-z])([a-z 0-9]){3,12}$" > /dev/null 2>&1
+while echo "$USERNAME" | egrep -v "[a-z][0-9a-z_]{2,11}$" > /dev/null 2>&1
 do
 	echo "You must enter a valid username - starts with lowercase and is in lowercase, digits and lowercase only!"
 	echo "Enter a valid username: "
